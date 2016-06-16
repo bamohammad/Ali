1b04f4996058753f5d88e78aaa7b4e459b9c4d1d
ClickappsUser3 <ClickappsUser3@clickappsuser3s-imac.local>
2016-06-16 09:42:21 +0530

    Initial Commit

---
+++ Scroll View/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Scroll View
+//
+//  Created by ClickappsUser3 on 16/06/16.
+//  Copyright © 2016 ClickappsUser3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Scroll ViewTests/Scroll_ViewTests.m
@@ -0,0 +1,39 @@
+//
+//  Scroll_ViewTests.m
+//  Scroll ViewTests
+//
+//  Created by ClickappsUser3 on 16/06/16.
+//  Copyright © 2016 ClickappsUser3. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Scroll_ViewTests : XCTestCase
+
+@end
+
+@implementation Scroll_ViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
