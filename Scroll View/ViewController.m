6b2812e0f919ea5407552cf9692d6b7d80e209e9
bamohammad <bamohammad@clickapps.co>
2016-08-02 15:35:55 +0530

    first commit

--- Scroll View/ViewController.m
+++ Scroll View/ViewController.m
@@ -10,15 +10,49 @@
 
 @interface ViewController ()
 
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSArray *images=[NSArray arrayWithObjects:@"1art4.jpg",@"1art5.jpg",@"1art6.jpg",@"1art7.jpg",nil];
+    UIScrollView* scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
+    scrollView.backgroundColor = [UIColor redColor];
+    scrollView.scrollEnabled = YES;
+    scrollView.pagingEnabled = YES;
+    scrollView.showsVerticalScrollIndicator = YES;
+    //scrollView.showsHorizontalScrollIndicator = YES;
+    scrollView.contentSize = CGSizeMake([UIScreen mainScreen].bounds.size.width * images.count, [UIScreen mainScreen].bounds.size.height);
+    [self.view addSubview:scrollView];
+
+    
+    
+    for (int i=0; i<images.count; i++) {
+        UIImageView *image=[[UIImageView alloc] initWithFrame:CGRectMake( [UIScreen mainScreen].bounds.size.width*i, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
+        image.image=[UIImage imageNamed:images[i]];
+            [scrollView addSubview:image];
+    }
+
 }
 
+-(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+    NSLog(@"Did end decelerating");
+}
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView{
+        NSLog(@"Did scroll");
+}
+-(void)scrollViewDidEndDragging:(UIScrollView *)scrollView
+                 willDecelerate:(BOOL)decelerate{
+    NSLog(@"Did end dragging");
+}
+-(void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView{
+    NSLog(@"Did begin decelerating");
+}
+-(void)scrollViewWillBeginDragging:(UIScrollView *)scrollView{
+    NSLog(@"Did begin dragging");
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
